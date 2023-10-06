fe42e3c8c31c9e25b634a15aa579817d19c4b444
daniel borko <dan.borko@seznam.cz>
2023-10-06 09:20:41 +0200

    zmena

--- res/js/main.js
+++ res/js/main.js
@@ -7,5 +7,5 @@ cookie.onclick = () => {
     //zvednout cislo o 1
 numberOfCookies = numberOfCookies + 1 
     //zobrazit v odstavci
-    counter.innerText = "Zabitých nechutných smurf catů :" + numberOfCookies;
+    counter.innerText = "Zabitých smurf catů :" + numberOfCookies;
 }
